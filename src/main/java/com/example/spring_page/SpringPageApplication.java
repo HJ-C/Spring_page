82b422cf3833b01bed0e5c8e17060d29d6bc43e4
HJ-C <swch56@naver.com>
2022-10-21 02:59:16 +0900

    #1 commit

---
+++ src/main/java/com/example/spring_page/SpringPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPageApplication.class, args);
+    }
+
+}
